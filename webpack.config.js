209f4f866f08c0cf7a16ab061635d2e2acd58419
Stefan McCready <stefan.mccready@sky.uk>
2019-10-20 16:42:36 +0100

    removed xml documents

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, './'),
+        filename: 'index.js'
     },
     optimization: {
       minimize: true,
